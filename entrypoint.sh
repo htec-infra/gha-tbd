144c9b82e910d8dcccb986b3e7878a2b244db516
Jovica Zlatanovic <jovica.zlatanovic@htecgroup.com>
2021-09-01 17:25:44 +0200

    Implement 'Prepare App Release' method

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,26 @@ echo "######"
 echo ""
 
 OP="${1}"
+PARAMS="${2}"
+
+if [[ "${OP}" == "help" ]]; then
+  echo ""
+  echo "Supported operations:"
+  echo "  - get_version: "
+  echo "  - prepare_release: "
+  echo ""
+fi
 
 if [[ "${OP}" == "get_version" ]]; then
-  echo "Extracting repo data"
+  echo ">>> Extracting repo data"
   /bin/bash /app/repo_data.sh
 
   exit 0
 fi
 
+if [[ "${OP}" == "prepare_release" ]]; then
+  echo ">>> Preparing Release branch"
+  /bin/bash /app/prepare_app_release.sh ${PARAMS}
+
+  exit 0
+fi
